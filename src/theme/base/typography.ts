b323f73e4a995c061e63c728f0109fbfa507cf9d
Jacob LeCoq <lecoqjacob@gmail.com>
2023-02-20 19:22:01 -0600

    feat: add NovaSquare font

--- src/theme/base/typography.ts
+++ src/theme/base/typography.ts
@@ -1,5 +1,6 @@
 const typography = {
   electro: ['Electro'],
+  'nova-square': ['NovaSquare'],
   sans: ['Electro', 'Roboto', 'sans-serif'],
   serif: ['Electro', 'Roboto Slab', 'serif'],
   body: ['Electro', 'Roboto', 'sans-serif'],
